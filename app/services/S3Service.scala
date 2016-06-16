319e37553371eb9d928986c535eb79a97af130cd
Priyanka P <ppn3633@Pree-Mac.local>
2016-06-16 14:53:36 +0530

    Refactoring Code

--- app/services/S3Service.scala
+++ app/services/S3Service.scala
@@ -1,58 +1,53 @@
 package services
 
 import java.io._
+import javax.inject.Inject
 
 import com.amazonaws.services.s3.model.{ObjectListing, S3Object}
-import utils.{Constants, S3Client}
+import utils.Constants.{TARGET_DIRECTORY, bucketName}
+import utils.{Constants, FileUtils, S3Client}
+
 import scala.collection.JavaConverters._
 
-trait BucketingFile {
-
-  def fileName( path :String ) : String = {
-    val x = path.substring(path.lastIndexOf("/")+1)
-    x
-  }
-
-  def getParent( path : String ) : String = {
-    path.substring(0, path.lastIndexOf("/"))
-  }
-
-}
-
-class S3Service(s3Client : S3Client = new S3Client) extends BucketingFile {
-
-  def getFile( fileKey :String ): Unit ={
-    val outputStream = new BufferedOutputStream(new FileOutputStream(Constants.TARGET_DIRECTORY + fileName(fileKey)))
-    try{
-      val s3Object : S3Object = s3Client.getS3Client.getObject(Constants.bucketName,fileKey)
-      val inputStream = s3Object.getObjectContent
-      Iterator.continually (inputStream.read).takeWhile (-1 !=).foreach (outputStream.write)
-    }catch{
-      case e : Exception => throw e ;
-    }finally{
-      outputStream.close()
-    }
-  }
+class S3Service @Inject()(s3Client : S3Client){
 
   def getBucketFiles(): Unit ={
-    val objectListings = getAllObjectListing()
-    objectListings.flatMap(_.getObjectSummaries.asScala.map(_.getKey)).filter(p=> p.endsWith(".xml")).map(getFile(_))
+    val objectListings: List[ObjectListing] = getAllS3Objects()
+    objectListings.flatMap(_.getObjectSummaries.asScala.map(_.getKey)).filter(fileKey=> fileKey.endsWith(".xml")).map(readAndWriteFile(_))
   }
 
-  private def getAllObjectListing() : List[ObjectListing] = {
-    val objects: ObjectListing = s3Client.getS3Client.listObjects(Constants.bucketName)
-     getAllObjectListingRecursively(List(objects))
+  private def getAllS3Objects() : List[ObjectListing] = {
+    val objects: ObjectListing = s3Client.getS3Client.listObjects(bucketName)
+    getS3ObjectRecursively(List(objects))
   }
 
-  private def getAllObjectListingRecursively(output: List[ObjectListing]): List[ObjectListing] = {
+  private def getS3ObjectRecursively(output: List[ObjectListing]): List[ObjectListing] = {
     if(output.head.isTruncated) {
-      getAllObjectListingRecursively(s3Client.getS3Client.listNextBatchOfObjects(output.head) :: output)
+      getS3ObjectRecursively(s3Client.getS3Client.listNextBatchOfObjects(output.head) :: output)
     } else {
       output
     }
   }
+
+  private def readAndWriteFile(filePath:String): Unit ={
+    val targetDestination = TARGET_DIRECTORY + filePath
+    val outputStream = new BufferedOutputStream(new FileOutputStream(targetDestination))
+
+    FileUtils.createDirs(filePath, targetDestination)
+
+     try{
+      val s3Object : S3Object = s3Client.getS3Client.getObject(bucketName,filePath)
+      val inputStream = s3Object.getObjectContent
+      Iterator.continually (inputStream.read).takeWhile (-1 !=).foreach (outputStream.write)
+    }catch{
+      case e : IOException => println("Unable to write file")
+    }finally {
+      outputStream.close()
+    }
+  }
+
 }
 
-object s extends  App {
-  new S3Service().getBucketFiles()
+object Test extends  App {
+  new S3Service(new S3Client).getBucketFiles()
 }
--- app/utils/S3Client.scala
+++ app/utils/S3Client.scala
@@ -3,9 +3,7 @@ package utils
 import com.amazonaws.auth.BasicAWSCredentials
 import com.amazonaws.services.s3.AmazonS3Client
 
-/**
-  * Created by ppn3633 on 6/14/16.
-  */
+
 class S3Client {
   def getS3Client = {
     val yourAWSCredentials : BasicAWSCredentials = new BasicAWSCredentials(Constants.AWS_ACCESS_KEY, Constants.AWS_SECRET_KEY)
